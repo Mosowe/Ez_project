04f9348a36112a419ee017f9bfb529c4b68bb8ef
Mosowe <630834853@qq.com>
2017-11-23 11:34:14 +0800

    ...

--- src/main.js
+++ src/main.js
@@ -65,9 +65,23 @@ const router = new VueRouter({
         requireAuth: true  // 添加该字段，表示进入这个路由是需要登录的
       },
       components: {
-        content: login
+        content: resolve => require(['components/login/login.vue'], resolve)
         // footer: footer
       }
+      // beforeEach: (to, from, next) => {
+      //   if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
+      //     if (store.state.token) {  // 通过vuex state获取当前的token是否存在
+      //       next();
+      //     } else {
+      //       next({
+      //         path: '/login',
+      //         query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
+      //       })
+      //     }
+      //   } else {
+      //     next();
+      //   }
+      // }
     },
     {
       path: '/wordDetail/:id',
@@ -99,22 +113,6 @@ const router = new VueRouter({
     }
   ]
 });
-// router.beforeEach((to, from, next) => {
-//   if (to.meta.requireAuth) {  // 判断该路由是否需要登录权限
-//     if (store.state.token) {  // 通过vuex state获取当前的token是否存在
-//       next();
-//     };
-//     else {
-//       next({
-//         path: '/login',
-//         query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
-//       })
-//     }
-//   }
-//   else {
-//     next();
-//   }
-// })
 
 router.push('/index');
 
